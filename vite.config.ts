d2f7af577014b1bce6328c5b3bdf23ae72763d46
zeeshanahmed17 <zeeshanahmed5003@gmail.com>
2025-04-11 18:02:08 +0530

    v

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
